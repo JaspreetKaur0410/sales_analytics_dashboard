0be11b2d6073d84d2b21377eec5d371ce245f499
Jaspreet Kaur <78718558+JaspreetKaur0410@users.noreply.github.com>
2023-11-01 12:08:42 -0400

    Update query.py

--- query.py
+++ query.py
@@ -19,7 +19,7 @@ PASSWORD="Ja1sitaram"
 # token = client.generate_db_auth_token(DBHostname=ENDPOINT, Port=PORT, DBUsername=USER, Region=REGION)
 
 try:
-    conn =  pymysql.connect(host=ENDPOINT, user=USER, passwd=PASSWORD, port=PORT, database=DBNAME, ssl_ca='SSLCERTIFICATE')
+    conn =  pymysql.connect(host=ENDPOINT, user=USER, passwd=PASSWORD, port=PORT, database=DBNAME)
     cur = conn.cursor()
     cur.execute("""select * from insurance order by id asc""")
     query_results = cur.fetchall()
@@ -28,7 +28,7 @@ except Exception as e:
     print("Database connection failed due to {}".format(e))               
 
 def view_all_data():
-    conn =  pymysql.connect(host=ENDPOINT, user=USER, passwd=PASSWORD, port=PORT, database=DBNAME, ssl_ca='SSLCERTIFICATE')
+    conn =  pymysql.connect(host=ENDPOINT, user=USER, passwd=PASSWORD, port=PORT, database=DBNAME)
     cur = conn.cursor()
     cur.execute("""select * from insurance order by id asc""")
     query_results = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
