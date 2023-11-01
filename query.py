135762f3c009f25944df005c22382d58d8c8c182
Jaspreet Kaur <78718558+JaspreetKaur0410@users.noreply.github.com>
2023-11-01 11:54:32 -0400

    Update query.py

--- query.py
+++ query.py
@@ -4,38 +4,34 @@ import sys
 import boto3
 import os
 
-# ENDPOINT="database-2.cuzlxxs7vcah.us-east-1.rds.amazonaws.com"
-# PORT=3306
-# USER="admin"
-# REGION="us-east-1"
-# DBNAME="analytics_db"
-# PASSWORD="Ja1sitaram"
-# os.environ['LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN'] = '1'
+ENDPOINT="database-2.cuzlxxs7vcah.us-east-1.rds.amazonaws.com"
+PORT=3306
+USER="admin"
+REGION="us-east-1"
+DBNAME="analytics_db"
+PASSWORD="Ja1sitaram"
+os.environ['LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN'] = '1'
 
 #gets the credentials from .aws/credentials
-session = boto3.Session(
-    aws_access_key_id=st.secrets["aws_access_key_id"],
-    aws_secret_access_key=st.secrets["aws_secret_access_key"],
-    region_name=st.secrets["REGION"]
-)
-# client = session.client('rds')
-# token = client.generate_db_auth_token(DBHostname=ENDPOINT, Port=PORT,passwd=PASSWORD, DBUsername=USER, Region=REGION)
-# print("*************"+token)
+session = boto3.Session(profile_name='default')
+client = session.client('rds')
+
+token = client.generate_db_auth_token(DBHostname=ENDPOINT, Port=PORT, DBUsername=USER, Region=REGION)
+
 try:
-    conn =  pymysql.connect(host=st.secrets["ENDPOINT"], user=st.secrets["USER"], passwd=st.secrets["PASSWORD"], port=st.secrets["PORT"], 
-                                    database=st.secrets["DBNAME"])
+    conn =  pymysql.connect(host=ENDPOINT, user=USER, passwd=token, port=PORT, database=DBNAME, ssl_ca='SSLCERTIFICATE')
     cur = conn.cursor()
-    cur.execute("""select * from insurance order by id asc""")
+    cur.execute("""SELECT now()""")
     query_results = cur.fetchall()
     print(query_results)
 except Exception as e:
-    print("Database connection failed due to {}".format(e))                  
+    print("Database connection failed due to {}".format(e))               
 
 def view_all_data():
-    conn =  pymysql.connect(host=st.secrets["ENDPOINT"], user=st.secrets["USER"], passwd=st.secrets["PASSWORD"], port=st.secrets["PORT"], database=st.secrets["DBNAME"])
+    conn =  pymysql.connect(host=ENDPOINT, user=USER, passwd=token, port=PORT, database=DBNAME, ssl_ca='SSLCERTIFICATE')
     cur = conn.cursor()
-    cur.execute("""select * from insurance order by id asc""")
-    query_results = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+    cur.execute("""SELECT now()""")
+    query_results = cur.fetchall()
     print(query_results)
     return query_results
     
