59809166f8627cce115aab3fb9ef987eff73396c
Jaspreet Kaur <78718558+JaspreetKaur0410@users.noreply.github.com>
2023-11-01 13:44:34 -0400

    Update home.py

--- home.py
+++ home.py
@@ -147,7 +147,9 @@ sideBar()
 hide_st_style=""" 
 
 <style>
-#MainMenu {visibility:hidden;}
+#MainMenu {
+  visibility: hidden;
+}
 footer {visibility:hidden;}
 header {visibility:hidden;}
 </style>
