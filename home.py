9d2ae8308865a494548b6723f81c4a490d44c26c
Jaspreet Kaur <78718558+JaspreetKaur0410@users.noreply.github.com>
2023-11-01 13:46:51 -0400

    Update home.py

--- home.py
+++ home.py
@@ -6,18 +6,7 @@ import time
 from streamlit_option_menu import option_menu
 from numerize.numerize import numerize
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+
 
 st.set_page_config(page_title="Analytical_Dashboard", page_icon="🔔", layout="wide")
 
